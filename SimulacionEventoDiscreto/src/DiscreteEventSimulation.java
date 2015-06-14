8b64f3fb35aeabcc0d4e3fa9d210660f9d969fa8
afarmijos <afarmijos@gmail.com>
2015-06-14 14:18:39 -0500

    Casi terminado
    casi terminado

--- SimulacionEventoDiscreto/src/DiscreteEventSimulation.java
+++ SimulacionEventoDiscreto/src/DiscreteEventSimulation.java
@@ -3,19 +3,30 @@ public class DiscreteEventSimulation {
 
 	public static void main(String[] args) {
 		
-		
 		Simulation s = new Simulation();
 		
+		if ("random".equals(args[0])) {
+			
+			s.setup(); // setup simulation;
+			s.run(10000); // run of simulation
+			System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());
+			
+		}
+			
+		if ("trace".equals(args[0])) {				
+			s.ejecutarSimulacionTraceDriven(args[1]);
+			System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());				
+		}
 		
-		//s.setup(); // setup simulation;
+		if ("procesarResultado".equals(args[0])) {				
+			s.procesarResultado(args[1], args[2]);				
+		}		
 		
-		//s.run(10000); // run of simulation
+		if ("generarDriven".equals(args[0])) {				
+			s.generarEntradaDriven(args[1], args[2]);				
+		}
 		
-		
-		s.ejecutarSimulacionTraceDriven("C:\\Users\\Alex\\Google Drive\\PHD\\Clases\\SistemasOperativosAvanzados\\DeberDiscreteEventSimulation\\Entrada12.txt");
-		
-		
-		System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());
+				
 	}
 
 }
