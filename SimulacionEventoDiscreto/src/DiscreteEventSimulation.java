2bdf41fc97b573aa73b201166711e4bca273cc05
afarmijos <afarmijos@gmail.com>
2015-06-15 16:47:11 -0500

    prueba

--- SimulacionEventoDiscreto/src/DiscreteEventSimulation.java
+++ SimulacionEventoDiscreto/src/DiscreteEventSimulation.java
@@ -9,13 +9,13 @@ public class DiscreteEventSimulation {
 			
 			s.setup(); // setup simulation;
 			s.run(10000); // run of simulation
-			System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());
+			//System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());
 			
 		}
 			
 		if ("trace".equals(args[0])) {				
 			s.ejecutarSimulacionTraceDriven(args[1]);
-			System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());				
+			//System.out.println("El tiempo promedio fuera de servicio es: "+s.calcularTiempoPromedioDowntime());				
 		}
 		
 		if ("procesarResultado".equals(args[0])) {				
